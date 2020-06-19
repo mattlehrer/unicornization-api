b3acf969767469733114e02dad03e72af87572c3
Matt Lehrer <lehrerm@gmail.com>
2020-06-19 15:06:47 -0700

    change pg env vars

--- src/config/validation-schema.ts
+++ src/config/validation-schema.ts
@@ -10,9 +10,9 @@ export default Joi.object({
   //  Database
   DB_HOSTNAME: Joi.string().required(),
   DB_PORT: Joi.number().required(),
-  DB_NAME: Joi.string().required(),
-  DB_USERNAME: Joi.string().required(),
-  DB_PASSWORD: Joi.string().required(),
+  POSTGRES_DB: Joi.string().required(),
+  POSTGRES_USER: Joi.string().required(),
+  POSTGRES_PASSWORD: Joi.string().required(),
   TYPEORM_SYNC: Joi.boolean().default(false),
 
   // JWT
